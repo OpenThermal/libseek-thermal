955d10c8faa5697db758d4a1ffdb95cee03391d8
fnoop <fnoopdogg@gmail.com>
2017-05-24 16:39:26 +0100

    Add seek_viewer utility

---
+++ bin/seek_viewer.cpp
@@ -0,0 +1,181 @@
+// Seek Thermal Viewer/Streamer
+// http://github.com/fnoop/maverick
+
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include "seek.h"
+#include "SeekCam.h"
+#include <iostream>
+#include <string>
+#include <signal.h>
+#include <memory>
+#include "args.hxx"
+
+using namespace cv;
+using namespace LibSeek;
+
+// Setup sig handling
+static volatile sig_atomic_t sigflag = 0;
+void handle_sig(int sig) {
+    sigflag = 1;
+}
+
+// Function to process a raw (corrected) seek frame
+void process_frame(Mat &inframe, Mat &outframe, float scale, int colormap, int rotate) {
+        Mat frame_g8, frame_g16; // Transient Mat containers for processing
+
+        normalize(inframe, frame_g16, 0, 65535, NORM_MINMAX);
+
+        // Convert seek CV_16UC1 to CV_8UC1
+        frame_g8 = frame_g16;
+        frame_g8.convertTo( frame_g8, CV_8UC1, 1.0/256.0 );
+        
+        // Rotate image
+        if (rotate == 90) {
+            transpose(frame_g8, frame_g8);
+            flip(frame_g8, frame_g8, 1);
+        } else if (rotate == 180) {
+            flip(frame_g8, frame_g8, -1);
+        } else if (rotate == 270) {
+            transpose(frame_g8, frame_g8);
+            flip(frame_g8, frame_g8, 0);
+        }
+        
+        // Resize image: http://docs.opencv.org/3.2.0/da/d54/group__imgproc__transform.html#ga5bb5a1fea74ea38e1a5445ca803ff121
+        // Note this is expensive computationally, only do if option set != 1
+        if (scale != 1.0)
+            resize(frame_g8, frame_g8, Size(), scale, scale, INTER_LINEAR);
+
+        // Apply colormap: http://docs.opencv.org/3.2.0/d3/d50/group__imgproc__colormap.html#ga9a805d8262bcbe273f16be9ea2055a65
+        if (colormap != -1) {
+            applyColorMap(frame_g8, outframe, colormap);
+        } else {
+            outframe = frame_g8;
+        }
+}
+
+int main(int argc, char** argv)
+{
+    // Setup arguments for parser
+    args::ArgumentParser parser("Seek Thermal Viewer");
+    args::HelpFlag help(parser, "help", "Display this help menu", {'h', "help"});
+    args::Flag verbose(parser, "verbose", "Verbose", {'v', "verbose"});
+    args::ValueFlag<string> _output(parser, "output", "Output Stream", {'o', "output"});
+    args::ValueFlag<int> _fps(parser, "fps", "Video Output FPS - Kludge factor", {'f', "fps"});
+    args::ValueFlag<float> _scale(parser, "scaling", "Output Scaling - multiple of original image", {'s', "scale"});
+    args::ValueFlag<int> _colormap(parser, "colormap", "Color Map", {'c', "colormap"});
+    args::ValueFlag<int> _rotate(parser, "rotate", "Rotation", {'r', "rotate"});
+    args::ValueFlag<string> _camtype(parser, "camtype", "Seek Thermal Camera Model - seek or seekpro", {'t', "camtype"});
+
+    // Parse arguments
+    try
+    {
+        parser.ParseCLI(argc, argv);
+    }
+    catch (args::Help)
+    {
+        std::cout << parser;
+        return 0;
+    }
+    catch (args::ParseError e)
+    {
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+        return 1;
+    }
+    catch (args::ValidationError e)
+    {
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+        return 1;
+    }
+    float scale = 1.0;
+    if (_scale)
+        scale = args::get(_scale);
+    string output = "window";
+    if (_output)
+        output = args::get(_output);
+    string camtype = "seek";
+    if (_camtype)
+        camtype = args::get(_camtype);
+    // 7fps seems to be about what you get from a seek thermal compact
+    // Note: fps doesn't influence how often frames are processed, just the VideoWriter interpolation
+    int fps = 7;
+    if (_fps)
+        fps = args::get(_fps);
+    // Colormap int corresponding to enum: http://docs.opencv.org/3.2.0/d3/d50/group__imgproc__colormap.html
+    int colormap = -1;
+    if (_colormap)
+        colormap = args::get(_colormap);
+    // Rotate default is landscape view to match camera logo/markings
+    int rotate = 270;
+    if (_rotate)
+        rotate = args::get(_rotate);
+
+    // Register signals
+    signal(SIGINT, handle_sig);
+    signal(SIGTERM, handle_sig);
+    
+    // Setup seek camera
+    LibSeek::SeekCam* seek;
+    LibSeek::SeekThermalPro seekpro;
+    LibSeek::SeekThermal seekclassic;
+    if (camtype == "seekpro")
+        seek = &seekpro;
+    else
+        seek = &seekclassic;
+
+    if (!seek->open()) {
+        cout << "Error accessing camera" << endl;
+        return 1;
+    }
+    
+    // Mat containers for seek frames
+    Mat seekframe, outframe;
+
+    // Retrieve a single frame, resize to requested scaling value and then determine size of matrix
+    //  so we can size the VideoWriter stream correctly
+    seek->retrieve(seekframe);
+    process_frame(seekframe, outframe, scale, colormap, rotate);
+
+    // Create an output object, if output specified then setup the pipeline unless output is set to 'window'
+    VideoWriter writer;
+    if (output != "window") {
+        writer.open(output, 0, fps, Size(outframe.cols, outframe.rows), true);
+        if (!writer.isOpened()) {
+            cerr << "Error can't create video writer" << endl;
+            return 1;
+        } else {
+            cout << "Video stream created, dimension: " << outframe.cols << "x" << outframe.rows << ", fps:" << fps << endl;
+        }
+    }
+    
+    // Main loop to retrieve frames from camera and output
+    while (true) {
+        
+        // If signal for interrupt/termination was received, break out of main loop and exit
+        if (sigflag || !seek->grab()) {
+            if (!seek->grab())
+                cout << "No more frames from camera, exiting" << endl;
+            if (sigflag)
+                cout << "Break signal detected, exiting" << endl;
+            break;
+        }
+
+        // Retrieve frame from seek and process
+        seek->retrieve(seekframe);
+        process_frame(seekframe, outframe, scale, colormap, rotate);
+
+        if (output == "window") {
+            imshow("SeekThermal", outframe);
+            char c = waitKey(10);
+            if (c == 's') {
+                waitKey(0);
+            }
+        } else {
+            writer << outframe;
+        }
+
+    }
+    
+}
