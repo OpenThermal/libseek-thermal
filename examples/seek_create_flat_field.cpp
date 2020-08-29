c82bb19232e790f5ff0fb3489ca41578be5fbbea
Alexander Gee <bostwickenator@gmail.com>
2020-08-29 15:24:09 -0500

    Remove usage of boost

--- examples/seek_create_flat_field.cpp
+++ examples/seek_create_flat_field.cpp
@@ -4,72 +4,8 @@
  */
 #include <opencv2/highgui/highgui.hpp>
 #include "seek.h"
-#include "boost/program_options.hpp"
 #include <iostream>
-
-namespace po = boost::program_options;
-
-static po::options_description get_description()
-{
-    po::options_description description;
-
-    description.add_options()
-        ("help,h", "Show this help")
-        ("cam,c", po::value<std::string>()->default_value(std::string("seekpro")), "Camera type: choises: seekpro|seek"
-                                                                " -> default is 'seekpro'")
-        ("smoothing,s", po::value<int>()->default_value(100), "Number of images to average before creating an output image")
-        ("outfile", po::value<std::string>()->required(), "Name of the output file");
-
-    return description;
-}
-
-static po::variables_map parse_options(int argc, char** argv)
-{
-    po::positional_options_description positional_description;
-    positional_description.add("outfile", -1);
-    po::variables_map variables_map;
-
-    auto description = get_description();
-    po::store(po::command_line_parser(argc, argv)
-                    .options(description)
-                    .positional(positional_description)
-                    .run(), variables_map);
-
-    if (variables_map.count("help") > 0) {
-        return variables_map;
-    }
-
-    po::notify(variables_map);
-    return variables_map;
-}
-
-static void print_help(const char* program_name)
-{
-    std::cout << "Usage: " << program_name << " [options] outputfilename" << std::endl;
-    std::cout << get_description() << std::endl;
-}
-
-static bool process_command_line_options(int argc, char **argv, po::variables_map& options)
-{
-    try
-    {
-        options = parse_options(argc, argv);
-    }
-    catch (po::error &e)
-    {
-        std::cerr << "ERROR: " << e.what() << std::endl;
-        print_help(argv[0]);
-        return false;
-    }
-
-    if (options.find("help") != options.end())
-    {
-        print_help(argv[0]);
-        return false;
-    }
-
-    return true;
-}
+#include "args.h"
 
 int main(int argc, char** argv)
 {
@@ -78,26 +14,76 @@ int main(int argc, char** argv)
     LibSeek::SeekThermalPro seekpro;
     LibSeek::SeekThermal seek;
     LibSeek::SeekCam* cam;
-    po::variables_map options;
 
-    if (!process_command_line_options(argc, argv, options))
-        return -1;
+    args::ArgumentParser parser("Create Flat Frame");
+    args::HelpFlag help(parser, "help", "Display this help menu", { 'h', "help" });
+    args::ValueFlag<std::string> _output(parser, "outfile", "Name of the file to write to - default flat_field.png", { 'o', "outfile" });
+    args::ValueFlag<int> _smoothing(parser, "smoothing", "Smoothing factor, number of frames to collect and average - default 100", { 's', "smoothing" });
+    args::ValueFlag<std::string> _camtype(parser, "camtype", "Seek Thermal Camera Model - seek or seekpro", { 't', "camtype" });
+    args::ValueFlag<int> _warmup(parser, "warmup", "Warmup, number of frames to discard before sampling - default 10", { 'w', "warmup" });
 
-    int smoothing = options["smoothing"].as<int>();
-    std::string outfile = options["outfile"].as<std::string>();
+    // Parse arguments
+    try {
+        parser.ParseCLI(argc, argv);
+    }
+    catch (args::Help) {
+        std::cout << parser;
+        return 0;
+    }
+    catch (args::ParseError e) {
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+        return 1;
+    }
+    catch (args::ValidationError e) {
+        std::cerr << e.what() << std::endl;
+        std::cerr << parser;
+        return 1;
+    }
 
-    if (options["cam"].as<std::string>() == "seekpro")
+    // Defaults 
+    int smoothing = 100;
+    if (_smoothing)
+        smoothing = args::get(_smoothing);
+
+    int warmup = 10;
+    if (_warmup)
+        warmup = args::get(_warmup);
+
+    std::string outfile = "flat_field.png";
+    if (_output)
+        outfile = args::get(_output);
+
+    std::string camtype = "seek";
+    if (_camtype)
+        camtype = args::get(_camtype);
+
+    // Init correct cam type
+    if (camtype == "seekpro") {
         cam = &seekpro;
-    else
+    }
+    else {
         cam = &seek;
+    }
 
     if (!cam->open()) {
-        std::cout << "failed to open seek cam" << std::endl;
+        std::cout << "failed to open " << camtype << " cam" << std::endl;
         return -1;
     }
 
-    for (i=0; i<smoothing; i++) {
+    for (i = 0; i < warmup; i++) {
+        if (!cam->grab()) {
+            std::cout << "no more LWIR img" << std::endl;
+            return -1;
+        }
+        cam->retrieve(frame_u16);
+        cv::waitKey(10);
+    }
 
+    std::cout << "warmup complete" << std::endl;
+
+    // Aquire frames
+    for (i = 0; i < smoothing; i++) {
         if (!cam->grab()) {
             std::cout << "no more LWIR img" << std::endl;
             return -1;
@@ -111,10 +97,10 @@ int main(int argc, char** argv)
         } else {
             avg_frame += frame;
         }
-
         cv::waitKey(10);
     }
 
+    // Average the collected frames
     avg_frame /= smoothing;
     avg_frame.convertTo(frame_u16, CV_16UC1);
 
